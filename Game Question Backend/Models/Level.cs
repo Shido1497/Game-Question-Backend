fc9222f55a2601e5cdbbe10b67bfa5a974fe8092
Shido1497 <shido1497@hotmail.com>
2022-04-19 08:54:33 -0500

    Se agregan Models

---
+++ Game Question Backend/Models/Level.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionGame.Models
+{
+    public class Level : EntityBase
+    {
+        public int Difficulty { get; set; }
+
+        public double Points { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
---
+++ Game Question Backend/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionGame.Models
+{
+    public class Player : EntityBase
+    {
+        public string Name { get; set; }
+        public List<Match> Matches { get; set; }
+    }
+}
