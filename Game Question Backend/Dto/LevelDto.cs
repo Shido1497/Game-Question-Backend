78e82b69f7965322e078a533139f4bb8fbaea05a
Shido1497 <shido1497@hotmail.com>
2022-04-19 08:19:00 -0500

    Se agregand Dto

---
+++ Game Question Backend/Dto/LevelDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionGame.Dto
+{
+    public class LevelBaselDto
+    {
+        public int Difficulty { get; set; }
+
+        public double Points { get; set; }
+    }
+
+    public class CreateLevelDto: LevelBaselDto
+    {
+    }
+
+    public class LevelDto : LevelBaselDto
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Game Question Backend/Dto/MatchDto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionGame.Dto
+{
+    public class MatchBaseDto
+    {
+        public int PlayerId { get; set; }
+    }
+    public class CreateMatchDto: MatchBaseDto
+    {
+    }
+
+
+    public class MatchDto
+    {
+        public int PlayerId { get; set; }
+        public Double TotalPoints { get; set; }
+        public int Round { get; set; }
+        public string Status { get; set; }
+        public bool CompleteGame { get; set; }
+    }
+
+    public class SendMatchRoundAnswerDto
+    {
+        public int MatchId { get; set; }
+        public int OptionId { get; set; }
+        public int RoundId { get; set; }
+        public int CategoryId { get; set; }
+    }
+
+    public class StartRoundDto
+    {
+        public int MatchId { get; set; }
+    }
+
+    public class StartRoundResponseDto
+    {
+        public int MatchId { get; set; }
+        public int RoundId { get; set; }
+        public int CurrentRound { get; set; }
+        public QuestionDto questionDto { get; set; }
+    }
+
+    public class RoundResponseBase
+    {
+        public Models.Enum.Status Status { get; set; }
+        public string StatusDescription { get; set; }
+        public bool CompleteGame { get; set; }
+        public double TotalPoints { get; set; }
+    }
+
+    public class SendMatchRoundAnswerResponseDto: RoundResponseBase
+    {
+
+    }
+
+    public class SendRetireDto
+    {
+        public int MatchId { get; set; }
+    }
+
+    public class SendRetireResponseDto : RoundResponseBase 
+    {
+    }
+}
